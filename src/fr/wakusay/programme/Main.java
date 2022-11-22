cedcadc42288624013ebd69a517fff2c2a47a830
maxence dorizon <mdorizon17@gmail.com>
2022-11-22 21:19:21 +0100

    les variables

--- src/fr/wakusay/programme/Main.java
+++ src/fr/wakusay/programme/Main.java
@@ -4,8 +4,30 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("hello world !!");
-
+		
+		
+		
+		//utilisation des variables pour une moyenne
+		System.out.println("");
+		
+		int note1 = 10;
+		int note2 = 20;
+		int note3 = 4;
+		
+		int moyenne = (note1 + note2 + note3)/3;
+		System.out.println("moyenne des notes " + moyenne);
+		
+		
+		//utilisation simple des variables
+		System.out.println("");
+		
+		String pseudo = "wakusay";
+		int age = 17;
+		
+		System.out.println(pseudo + " à " + age);
+		age++;
+		pseudo = "waku";
+		System.out.println(pseudo + " à maintenant " + age);
 	}
 
 }
