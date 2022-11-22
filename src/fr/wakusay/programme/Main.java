cec6e845a6463750a7e917a7acc43f62b7afe2ad
maxence dorizon <mdorizon17@gmail.com>
2022-11-22 21:57:54 +0100

    les conditions

--- src/fr/wakusay/programme/Main.java
+++ src/fr/wakusay/programme/Main.java
@@ -5,8 +5,39 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
+		//les conditions
+		System.out.println("");
+		
+		//switch
+		System.out.println("");
+		
+		int age2 = 11;
+		
+		switch(age2){
+			case 10:
+				System.out.println("10 ans");
+			break;
+			
+			case 11:
+				System.out.println("11 ans");
+			break;
+			
+			default: System.out.println("Cet age n'as pas de message");
+		}
 		
 		
+		//if / else / else if
+		System.out.println("");
+	
+		int money = 2000;
+		
+		if(money == 2000) {
+			System.out.println("oui");
+		} else {
+			System.out.println("non");
+		}
+		
+		 
 		//utilisation des variables pour une moyenne
 		System.out.println("");
 		
