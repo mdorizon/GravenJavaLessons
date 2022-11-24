1eaf7877cc03cec43c818947ebef73ee5b935f8d
maxence dorizon <mdorizon17@gmail.com>
2022-11-24 12:09:15 +0100

    boucle for

--- src/fr/wakusay/programme/Main.java
+++ src/fr/wakusay/programme/Main.java
@@ -4,7 +4,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		//boucle foreach
 		
+		
+		
+		// boucle for
+		
+		/**
+		for(int i = 1; i <= 100; i++) {
+			System.out.println(i);
+		}
+		*/
 		
 		
 		// les tableaux
